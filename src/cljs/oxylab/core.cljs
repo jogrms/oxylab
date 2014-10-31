f4af77770aa145f66b44bc3e3f62d4fd75b13af4
Ivan Babushkin <jogrms@gmail.com>
2014-10-31 17:19:22 +0400

    fix warning

--- src/cljs/oxylab/core.cljs
+++ src/cljs/oxylab/core.cljs
@@ -1,6 +1,5 @@
 (ns oxylab.core
   (:require [oxylab.model :as m]
-            [oxylab.view :as v]
             [oxylab.components :as cmp]
             [oxylab.state :as s]
             [cljs.reader :refer [read-string]]))
