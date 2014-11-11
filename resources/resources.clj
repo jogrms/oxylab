8f9afbe9520dad832f69920fb3f9ef0f70c458c6
Grandmothers <kostya-babushkin@yandex.ru>
2014-11-11 18:18:25 +0000

    resources.clj edited online with Bitbucket

--- resources/resources.clj
+++ resources/resources.clj
@@ -1,6 +1,6 @@
 {:detrit {:size 0
-          :max-size 1e4}
+          :max-size 100}
  :soil {:size 0.0
-        :max-size 100}
+        :max-size 50}
  :oxygen {:size 0.0
           :max-size 1e5}}
